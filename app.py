1dbd70b3735572bd148b138b8d30b1abac38a407
tanbir-hsain <tanbir.hsain@gmail.com>
2026-05-10 21:56:16 +0530

    updated backend and ML integration

--- app.py
+++ app.py
@@ -4,17 +4,21 @@ import re
 
 app = Flask(__name__)
 
+
 model = pickle.load(open("spam_model.pkl", "rb"))
 vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+
 
 def clean_text(text):
     text = text.lower()
     text = re.sub(r'[^a-z0-9 ]', '', text)
     return text
 
+
 @app.route("/")
 def home():
     return "Spam Model Running"
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -23,18 +27,21 @@ def predict():
         email_text = data.get("emailText")
 
         if not email_text:
-            return jsonify({"prediction": "ERROR"})
+            return jsonify({"prediction": "ERROR: No input"})
 
         email_text = clean_text(email_text)
+
         vector = vectorizer.transform([email_text])
         prediction = model.predict(vector)[0]
 
         result = "SPAM" if prediction == 1 else "HAM"
+
         return jsonify({"prediction": result})
 
     except Exception as e:
         print("ERROR:", e)
         return jsonify({"prediction": "ERROR"})
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)   # ✅ IMPORTANT CHANGE
+    app.run(host="0.0.0.0", port=5000)
--- email-extension/popup.js
+++ email-extension/popup.js
@@ -2,27 +2,21 @@ document.getElementById("checkBtn").addEventListener("click", function () {
 
     let text = document.getElementById("emailText").value;
 
-    fetch("http://localhost:8080/email/analyze", {
+    fetch("http://13.206.142.50:5000/predict", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-     body: JSON.stringify({ emailText: text })
-
+        body: JSON.stringify({ emailText: text })
     })
     .then(res => res.json())
     .then(data => {
-
         document.getElementById("result").innerText =
             "Prediction: " + data.prediction;
-
     })
     .catch(err => {
-
         console.error(err);
-
         document.getElementById("result").innerText = "ERROR";
-
     });
 
 });
